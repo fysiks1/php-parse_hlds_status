0f14dc1c8a651b3ecb91eef62cafa20fb0d8424a
Fysiks <fysiks1@gmail.com>
2020-02-23 21:26:20 -0600

    Robustify finding player data table Updated to account for potential variable white space

--- parse_hlds_status.php
+++ parse_hlds_status.php
@@ -1,37 +1,53 @@
 <?php
 
 /*
-	Parse HLDS status output
+*	Parse HLDS status output
 */
 function parse_hlds_status($StatusString)
 {
-	// Convert to an array
-	$StatusArray = preg_split("/(\r\n|\n|\r)/", $StatusString);
+	/*
+	*	Parse server parameters
+	*/
 	
-	// Parse server parameters
 	$matches = array();
-	preg_match("/hostname\s*:\s*(.*)$/", trim(array_shift($StatusArray)), $matches);
+	preg_match("/^hostname\s*:\s*(.*)$/im", $StatusString, $matches);
 	$out['hostname'] = $matches[1];
-	preg_match("/version\s*:\s*(.*)$/", trim(array_shift($StatusArray)), $matches);
+	preg_match("/version\s*:\s*(.*)$/im", $StatusString, $matches);
 	$out['version'] = $matches[1];
-	preg_match("/tcp\/ip\s*:\s*(.*)$/", trim(array_shift($StatusArray)), $matches);
+	preg_match("/tcp\/ip\s*:\s*(.*)/im", $StatusString, $matches);
 	$out['tcp/ip'] = $matches[1];
-	preg_match("/map\s*:\s*([\w-]+).*$/", trim(array_shift($StatusArray)), $matches);
+	preg_match("/map\s*:\s*([\w-]+).*$/im", $StatusString, $matches);
 	$out['map'] = $matches[1];
-	preg_match("/players\s*:\s*(\d+).*\((\d+).*\)$/", trim(array_shift($StatusArray)), $matches);
+	preg_match("/players\s*:\s*(\d+).*\((\d+).*\)$/im", $StatusString, $matches);
 	$PlayerCount = $matches[1];
 	$out['maxplayers'] = $matches[2];
 	
-	array_shift($StatusArray);
+	
+	/*
+	*	Parse player data
+	*	- locate # at begining of line to signify start of table
+	*	- locate second player count "\d+ users" which is the end of the table
+	*	- parse the table
+	*/
+	
+	// Isolate player data table
+	preg_match("/^#/im", $StatusString, $matches,  PREG_OFFSET_CAPTURE);
+	$iStart = $matches[0][1];
+	preg_match("/^(\d+)\s+users\s*$/im", $StatusString, $matches, PREG_OFFSET_CAPTURE);
+	$iEnd = $matches[0][1];
+	$PlayerDataString = trim(substr($StatusString, $iStart, $iEnd - $iStart));
+	
+	// Convert to an array
+	$PlayerDataArray = preg_split("/(\r\n|\n|\r)/", $PlayerDataString);
 	
 	// Parse header
-	$header = explode_by_whitespace(array_shift($StatusArray));
+	$header = explode_by_whitespace(array_shift($PlayerDataArray));
 
-	// Parse player table
-	$PlayerListString = array_slice($StatusArray, 0, $PlayerCount);
+	// Parse player data
+	$PlayerListString = array_slice($PlayerDataArray, 0, $PlayerCount);
 	$PlayerList = array_map('parse_player_line', $PlayerListString);
 	array_walk($PlayerList, '_combine_array', $header);
-	$out['Players'] = $PlayerList;
+	$out['players'] = $PlayerList;
 	
 	return $out;
 }
